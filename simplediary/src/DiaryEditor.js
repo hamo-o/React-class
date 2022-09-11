6e6be2d2f161e3594ac2f6b5e199002af8d0c9c7
hamo-o <yheel369@gmail.com>
2022-09-12 02:50:17 +0900

    React-DOM-조작

--- simplediary/src/DiaryEditor.js
+++ simplediary/src/DiaryEditor.js
@@ -1,6 +1,9 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 const DiaryEditor = () => {
+  const authorInput = useRef();
+  const contentInput = useRef();
+
   const [state, setState] = useState({
     author: '',
     content: '',
@@ -18,46 +21,38 @@ const DiaryEditor = () => {
   };
 
   const handleSubmit = () => {
-    console.log(state);
+    if (state.author.length < 1) {
+      alert('작성자는 최소 1글자 이상 입력해주세요');
+      // focus
+      authorInput.current.focus();
+      return;
+    }
+    if (state.content.length < 5) {
+      alert('일기 본문은 최소 5글자 이상 입력해주세요');
+      // focus
+      contentInput.current.focus();
+      return;
+    }
     alert('저장 성공');
   };
-
-  //   const [author, setAuthor] = useState('');
-  //   const [content, setContent] = useState('');
 
   return (
     <div className="DiaryEditor">
       <h2>오늘의 일기</h2>
       <div>
         <input
+          ref={authorInput}
           type="text"
           name="author"
-          //   value={author}
           value={state.author}
-          //   onChange={(e) => {
-          //     // setAuthor(e.target.value);
-          //     setState({
-          //       ...state,
-          //       //   content: state.content,
-          //       author: e.target.value,
-          //     });
-          //   }}
           onChange={handleChangeState}
         />
       </div>
       <div>
         <textarea
+          ref={contentInput}
           name="content"
-          //   value={content}
           value={state.content}
-          //   onChange={(e) => {
-          //     // setContent(e.target.value);
-          //     setState({
-          //       ...state,
-          //       //   author: state.author,
-          //       content: e.target.value,
-          //     });
-          //   }}
           onChange={handleChangeState}
         ></textarea>
       </div>
