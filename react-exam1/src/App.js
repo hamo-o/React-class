3ac811e26c3ffa40cfb92971af2866c23055a6c7
hamo-o <yheel369@gmail.com>
2022-09-11 23:41:13 +0900

    Create-React-App

---
+++ react-exam1/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  let name = "이현영";
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>안녕 리액트 {name}</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
