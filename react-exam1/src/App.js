5b8c74721beef8078349f529fb1392845e9ddb54
hamo-o <yheel369@gmail.com>
2022-09-12 00:23:18 +0900

    State

--- react-exam1/src/App.js
+++ react-exam1/src/App.js
@@ -2,39 +2,19 @@
 
 import MyHeader from "./MyHeader"
 import MyFooter from "./MyFooter";
+import Counter from "./Counter"
+
 
 import React from "react";
 
 function App() {
-  let name = "이현영";
-
-  const func = () => {
-    return "func";
-  }
-
-  const number = 5;
-
-  const style = {
-    App: {
-      backgroundColor: "black",
-    },
-    h2: {
-      color: "red",
-    },
-    bold_text: {
-      color: "green",
-    },
-  };
-
+ 
   return (
-    <div style={style.App}>
-      <MyHeader />
-        <h2 style={style.h2}>안녕 리액트 {func()}</h2>
-        <b style={style.bold_text} id="bold_text">
-          {number}는 : {number % 2 === 0 ? "짝수" : "홀수"}
-        </b>
-      <MyFooter />
-    </div>
+  <div>
+    <MyHeader />
+    <Counter />
+    <MyFooter />
+  </div>
   );
 } 
 
