a0ab857db46b31241f73aec713db73c03fb8604b
hamo-o <yheel369@gmail.com>
2022-09-12 04:33:58 +0900

    React-배열-3

--- simplediary/src/DiaryItem.js
+++ simplediary/src/DiaryItem.js
@@ -1,4 +1,4 @@
-const DiaryItem = ({ author, content, create_date, emotion, id }) => {
+const DiaryItem = ({ onDelete, author, content, create_date, emotion, id }) => {
   return (
     <div className="DiaryItem">
       <div className="info">
@@ -6,6 +6,16 @@ const DiaryItem = ({ author, content, create_date, emotion, id }) => {
         <div className="date">{new Date(create_date).toLocaleString()}</div>
       </div>
       <div className="content">{content}</div>
+      <button
+        onClick={() => {
+          console.log(id);
+          if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
+            onDelete(id);
+          }
+        }}
+      >
+        삭제하기
+      </button>
     </div>
   );
 };
