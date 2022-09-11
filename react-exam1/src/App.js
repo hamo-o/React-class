8d879b00facbb5bcb1bbbec34b541aef85e1a460
hamo-o <yheel369@gmail.com>
2022-09-12 00:06:41 +0900

    JSX

--- react-exam1/src/App.js
+++ react-exam1/src/App.js
@@ -1,15 +1,41 @@
-import "./App.css";
+// import "./App.css";
+
+import MyHeader from "./MyHeader"
+import MyFooter from "./MyFooter";
+
+import React from "react";
 
 function App() {
   let name = "이현영";
 
+  const func = () => {
+    return "func";
+  }
+
+  const number = 5;
+
+  const style = {
+    App: {
+      backgroundColor: "black",
+    },
+    h2: {
+      color: "red",
+    },
+    bold_text: {
+      color: "green",
+    },
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>안녕 리액트 {name}</h2>
-      </header>
+    <div style={style.App}>
+      <MyHeader />
+        <h2 style={style.h2}>안녕 리액트 {func()}</h2>
+        <b style={style.bold_text} id="bold_text">
+          {number}는 : {number % 2 === 0 ? "짝수" : "홀수"}
+        </b>
+      <MyFooter />
     </div>
   );
-}
+} 
 
 export default App;
