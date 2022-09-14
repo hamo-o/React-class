83d7cf79c9eac88a38316a77edb52c42ad2d9497
hamo-o <yheel369@gmail.com>
2022-09-14 18:26:23 +0900

    최적화-memo-3

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,8 +1,7 @@
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useEffect, useCallback, useMemo, useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
-import OptimizeTest from './OptimizeTest';
 
 function App() {
   const [data, setData] = useState([]);
@@ -31,7 +30,7 @@ function App() {
     getData();
   }, []);
 
-  const onCreate = (author, content, emotion) => {
+  const onCreate = useCallback((author, content, emotion) => {
     const created_date = new Date().getTime();
     const newItem = {
       author,
@@ -41,8 +40,8 @@ function App() {
       id: dataId.current,
     };
     dataId.current += 1;
-    setData([newItem, ...data]);
-  };
+    setData((data) => [newItem, ...data]); //이부분 다시보자.. 함수형 업데이트(?)
+  }, []);
 
   const onRemove = (targetId) => {
     console.log(`${targetId}(이)가 삭제되었습니다`);
@@ -70,7 +69,6 @@ function App() {
 
   return (
     <div className="App">
-      <OptimizeTest />
       <DiaryEditor onCreate={onCreate} />
       <div>전체 일기 : {data.length}</div>
       <div>기분 좋은 일기 개수 : {goodCount}</div>
