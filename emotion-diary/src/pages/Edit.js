a4335179168f955e7949547d5a1e395414077833
hamo-o <yheel369@gmail.com>
2022-09-19 19:29:15 +0900

    useSearchParams

--- emotion-diary/src/pages/Edit.js
+++ emotion-diary/src/pages/Edit.js
@@ -1,8 +1,24 @@
+import { useSearchParams } from 'react-router-dom';
+
 const Edit = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get('id');
+  const mode = searchParams.get('mode');
+
+  console.log(`id: ${id}, mode: ${mode}`);
+
   return (
     <div>
       <h1>Edit</h1>
       <p>이곳은 일기 수정 페이지 입니다.</p>
+      <button
+        onClick={() => {
+          setSearchParams({ who: 'ham' });
+        }}
+      >
+        QS 바꾸기
+      </button>
     </div>
   );
 };
